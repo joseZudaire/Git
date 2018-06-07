be23363c6e0ad2fc2c9a30f7ea354a08cef06408
joseZudaire <38169207+joseZudaire@users.noreply.github.com>
2018-06-07 18:10:13 -0300

    Update tp3v3.hs

--- tp3v3.hs
+++ tp3v3.hs
@@ -1,9 +1,12 @@
+type Jugador = (String, Bolillas)
 type Bolillas = [Int]
 
 duenioDelBingo = "Perez"
 ultimaBolilla = 17
 
+jugador1::Jugador
 jugador1  = ("Juan Perez", [5, 23, 35, 17, 56, 80])
+jugador2::Jugador
 jugador2  = ("Mariano Moreno", [5, 23, 35, 16, 56, 80])
 
 cantidadCorrectaNumeros = 6
@@ -17,18 +20,18 @@ cantidadNumeros bolillas = length bolillas == cantidadCorrectaNumeros
 ordenNumeros:: Bolillas -> Bool
 ordenNumeros bolillas = (maximum bolillas == last bolillas) && (minimum bolillas == head bolillas)
 
-jugadaValida::(String, Bolillas) -> Bool
+jugadaValida::Jugador -> Bool
 jugadaValida (nombre, bolillas) = ordenNumeros bolillas && cantidadNumeros bolillas
 
-tieneUltimaBolilla:: (String, Bolillas) -> Int -> Bool
+tieneUltimaBolilla:: Jugador -> Int -> Bool
 tieneUltimaBolilla (nombre, bolillas) indice | (last(take indice bolillas) == ultimaBolilla) = True
 											 | (indice == length bolillas) = False
 											 | otherwise = tieneUltimaBolilla (nombre, bolillas) (indice + 1)
 										 
-esFamiliar::(String, Bolillas) -> Bool
+esFamiliar::Jugador -> Bool
 esFamiliar (nombre, bolillas) = (drop cantidadLetrasApellido nombre == duenioDelBingo)
 
-ganaBingo::(String, Bolillas) -> Bool
+ganaBingo::Jugador -> Bool
 ganaBingo jugador = jugadaValida jugador && tieneUltimaBolilla jugador 1 && esFamiliar jugador
 
---Tuve problemas definiendo las funciones arriba, por eso está comentado. Parece ser que hay un problema con la definición de las Listas. Este jueves consultaría cómo se hace.
+--Ahora si funciona todo.
